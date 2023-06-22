197b5e1b1c6dec80272555087626b2c4045e7155
barbaraagra <babs.agra@gmail.com>
2023-06-22 11:20:17 +0100

    bonus 1

--- script.js
+++ script.js
@@ -35,6 +35,24 @@ if (hacker1.localeCompare(hacker2) === 1) {
     console.log(`What?! You both have the same name?`)
 }
 
-//Bonus
+//Bonus 
+let longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque laoreet mi vel posuere sodales. Nunc quis neque eleifend, convallis nulla sodales, venenatis urna. Duis tincidunt leo ex, vel vestibulum erat dictum eleifend. Morbi sed facilisis tortor. Nam quis laoreet lorem. Aliquam id sollicitudin odio. Praesent aliquam ligula tortor, quis tincidunt mauris faucibus vel. Maecenas sollicitudin egestas erat, non convallis velit bibendum nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras ut neque vel arcu vestibulum euismod. Pellentesque at ornare turpis. Mauris finibus rhoncus ligula, ac laoreet purus dignissim at. Quisque nec euismod arcu, et feugiat nulla.Nunc nibh leo, tincidunt sed dolor eget, dignissim maximus diam.Integer in dolor blandit, hendrerit odio ut, porttitor nibh.Vestibulum purus sem, rhoncus quis dolor vehicula, sagittis facilisis tellus.Donec eu quam tortor.Phasellus in auctor leo, eu venenatis leo.Suspendisse potenti. Duis sagittis nisl eros.Proin sit amet dictum tellus.Cras luctus, massa sit amet finibus interdum, elit tortor vulputate dui, vel sollicitudin ligula mauris at enim.Maecenas mi turpis, posuere in lorem eu, auctor malesuada ipsum.Curabitur sollicitudin erat et massa interdum auctor.Nulla varius purus a purus mattis tincidunt.Integer aliquet vel enim sit amet hendrerit.Vestibulum mattis dui eget magna porttitor vestibulum id vitae est.Nulla nisl elit, euismod a tempus interdum, malesuada malesuada neque.";
+let countText = 0;
+for (let i = 0; i <= longText.length; i++) {
+    if (longText[i] === ' ') {
+        countText++;
+        //    console.log(countText);
+    }
+}
+countText++;
+console.log(countText)
 
-
+//Make your program count the number of times the Latin word et appears.
+let count = 0;
+for (let i = 0; i <= longText.length; i++) {
+    if (longText.slice(i, i + 2) === 'et') {
+        count++;
+        //  console.log(count)
+    }
+}
+console.log(count);
